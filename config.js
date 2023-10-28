e69ec48c4eb24d457ce3c053bb83867ce41a7703
RockPower <rockpowerz@qq.com>
2023-10-28 23:24:06 +0800

    test001

--- config.js
+++ config.js
@@ -1,35 +1,262 @@
 window.g = {
     labels: {
-        welcome: "欢迎乘坐海南航空",
-        flightNo: "HU7668",
-        flightSuffix: "次航班",
-        flightFrom: "沈阳",
-        flightTo: "三亚",
-        wifiDesc: "客舱WIFI使用说明",
-        giftList: "精选伴手礼推荐",
-        more: "更多",
-        featuredGift: "精选伴手礼"
+        home: "首页",
+        business: "公司业务",
+        product: "产品介绍",
+        demo: "产品演示",
+        newsCenter: "新闻中心",
+        partners: "合作伙伴",
+        aboutUs: "关于我们",
+        contactUs: "联系我们",
+        companyAddressLabel: "公司地址：",
+        companyEmailLabel: "加入我们：",
+        companyEmail: "contact@weflyshare.com",
+        companyPhoneLabel: "联系我们：",
+        companyPhone: 4001819616,
+        companySloganLabel: "公司愿景：",
+        companySlogan: "成为全球领先的数字航空供应商",
+        demoTitle: "demo产品演示",
+        demoWords: "机上wifi链接 ......",
+        newsMore: "查看更多",
+        serviceAgreement: "服务协议",
+        qrCodeText1: "关注微信公众号",
+        qrCodeText2: "“飞享互联”",
+        qrCodeText3: "获取更多机上资讯",
+        copyright: ""
     },
-    cards: [
+    images: {
+        background: "banner-01.jpg",
+        companyLogo: "company-logo.png",
+        qrCode: "qr-code.png"
+    },
+    videos: {
+        demo: "demo.mp4"
+    },
+    businessData: [
         {
-            image: "https://lanhu-dds-backend.oss-cn-beijing.aliyuncs.com/merge_image/imgs/fd6c69d8d99f48ecb20e56cd9bfaaace_mergeImage.png",
-            label: "东北黄金玉米面条 6袋礼盒装",
-            currentPrice: "¥59.0",
-            originalPrice: "¥79.0",
-            url: "https://weixin.youzhipai.cn/productapi/28/80013833_product.html?t=1697787381925"
+            category: "航空互联网运营",
+            items: [
+                "航空互联网方案建设",
+                "系统集成",
+                "业务运营"
+            ]
         },
         {
-            image: "https://lanhu-dds-backend.oss-cn-beijing.aliyuncs.com/merge_image/imgs/18e3d6242f864c79839ca1205e4ecfe5_mergeImage.png",
-            label: "辽宁鞍山南果梨 20枚",
-            currentPrice: "¥69.0",
-            originalPrice: "¥70.0",
-            url: "https://weixin.youzhipai.cn/productapi/28/80007605_product.html?t=1697787381827"
-        },
-        {
-            image: "https://lanhu-dds-backend.oss-cn-beijing.aliyuncs.com/merge_image/imgs/328bac755a814b65840f77a0f6349d39_mergeImage.png",
-            label: "",
-            url: "https://weixin.youzhipai.cn/productapi/12/shop_home_page.html?shareName=%E5%AE%B6%E4%B9%A1%E5%91%B3%E9%81%93-%E5%85%AB%E6%96%B9%E7%BE%8E%E9%A3%9F%EF%BC%8C%E6%B1%87%E8%81%9A%E4%BA%8E%E6%AD%A4%E3%80%82&swiperW=0"
+            category: "航空数字化",
+            items: [
+                "航空产业数字化方案设计",
+                "系统集成",
+                "交付实施"
+            ]
         }
     ],
-    wifiDescUrl: "https://activepage.weflywifi.com/active/landingPage/1611400752086814721_0"
+    productData: [
+        {
+            title: "筋斗云同步系统",
+            words: "实现数字客舱资讯内容快速更新的算力基础",
+            pic: "product-pic-01.png"
+        },
+        {
+            title: "地面运营管理平台",
+            words: "飞享互联数字客舱系统的地面运营基础支撑平台",
+            pic: "product-pic-02.png"
+        },
+        {
+            title: "数字客舱支付系统",
+            words: "飞享互联数字客舱离线支付平台，打通空中无网购物环节",
+            pic: "product-pic-03.png"
+        },
+        {
+            title: "数字客舱娱乐系统",
+            words: "飞享互联数字客舱系统的核心组件，是用户登录的直接入口系统",
+            pic: "product-pic-04.png"
+        },
+        {
+            title: "数字客舱电商系统",
+            words: "飞享互联数字客舱电商运营平台",
+            pic: "product-pic-05.png"
+        },
+        {
+            title: "数字客舱广告系统",
+            words: "飞享互联数字客舱广告运营平台",
+            pic: "product-pic-06.png"
+        }
+    ],
+    newsData: [
+        {
+            url: "https://mp.weixin.qq.com/s/uqRA_Fggt2insGI0iVzFSw",
+            title: "飞享互联2周年｜机上WiFi互联网服务来了！在空中也能刷视频，聊微信？！",
+            words: "一顿淄博烧烤，让五湖四海的食客不远万里进淄赶“烤”，体验久违的人间烟火气；一部《去有风的地方》，治愈了多少在城市中忙碌奔波的打工人，在去往云南的向心之旅中寻找最初的自己；一条塞纳河，将宁静与温柔交融，漫步在河畔感受巴黎最极致的浪漫与城市风情。 6月出游季，无论你是想去山东、云南、海南等旅游胜地打卡祖国的大美河山，还是想直飞巴黎、布鲁塞尔、爱丁堡等欧洲文化之都，我们都将温暖陪伴，一场说走就走的旅行，即刻出发！",
+            thumb: "news_cola.png",
+            logo: "weixinLOGO.png"
+        },
+        {
+            url: "http://finance.ynet.com/2023/02/17/3589952t632.html",
+            title: "这个春节，6000多温暖航班+惊喜航班——飞享航空场景的宣发新势力",
+            words: "2023年春节档对于中国电影产业而言,可谓是一个全新的起点。春节7天总票房达67.58亿元,同比增长11.89%。观影人次超过1.29亿,同比增长13.16%。据数据统计显示,今年春节档已经成为中国影史总票房第二的春节档。类型多元、品质优秀的电影作品,观众高涨的观影热情,持续升温的话题热度,都展现出了我国电影市场正在强势复苏的态势。",
+            thumb: "news_chunjie.png",
+            logo: "beiqingwang.png"
+        },
+        {
+            url: "https://mp.weixin.qq.com/s/Fy2hwTpuCASs_CNYSkUeZA",
+            title: "​海南航空“满江红 红到家”春节限定电影主题航班正式启航",
+            words: "在即将迎来2023年新春佳节之际，海南航空联合猫眼电影、机队WiFi运营商飞享互联于1月18日北京—成都HU7147航班上，举办了张艺谋执导的春节档电影《满江红》的空中首次宣发主题活动“满江红 红到家”，邀请旅客在万米高空入局限时解谜，带给大家不一样的乘机体验。",
+            thumb: "news_manjianghong.png",
+            logo: "weixinLOGO.png"
+        }
+    ],
+    partnerData: [
+        {
+            title: "",
+            words: "资金、资源、运营；海量独家文娱内容无缝导入；顶级互联网运营能力和产业互联网经验；先进的数字航空全系列软件产品解决方案；腾讯云解决方案；数字化运营解决方案；",
+            pic: "partner-pic-01.png"
+        },
+        {
+            title: "",
+            words: "中国航电制造领军企业、国内首家FAA\\EASA\\CAAC适航认证企业，全球装机3000+；WiFi领域领先全球，创新的极简设计结构，易于改装和维护，质量稳定可靠；卡塔尔航空全机队使用多尼卡WiFi系统；国内海航、澳门航全机队使用；",
+            pic: "partner-pic-02.png"
+        },
+        {
+            title: "",
+            words: "中国第四大航空集团,650+架飞机，11家航司签署协议，占全国17%份额，年覆盖1.2亿人次高净值人群，15年独家运营权；先行、验证、示范",
+            pic: "partner-pic-03.png"
+        }
+    ],
+    partnerIcon: [
+        "partner-01.png",
+        "partner-02.png",
+        "partner-03.png",
+        "partner-04.png",
+        "partner-05.png",
+        "partner-06.png",
+        "partner-07.png",
+        "partner-08.png",
+        "partner-09.png",
+        "partner-10.png",
+        "partner-11.png",
+        "partner-12.png",
+        "partner-13.png",
+        "partner-14.png",
+        "partner-15.png",
+        "partner-16.png",
+        "partner-17.png",
+        "partner-18.png",
+        "partner-19.png",
+        "partner-20.png"
+    ],
+    bannerTips: [
+        {
+            name: "覆盖航空公司",
+            data: "11",
+            unit: "家"
+        },
+        {
+            name: "空地互联",
+            data: "28",
+            unit: "架"
+        },
+        {
+            name: "搭载数字客舱系统",
+            data: "超100",
+            unit: "架飞机"
+        },
+        {
+            name: "客舱互联网独家运营权",
+            data: "650",
+            unit: "架飞机"
+        },
+        {
+            name: "美好出行体验惠及",
+            data: "1.2亿",
+            unit: "+旅客"
+        }
+    ],
+    aboutUsData: [
+        {
+            title: "公司介绍",
+            words: "飞享互联航空科技（海南）有限公司成立于 2021 年 6 月，由腾讯、海航集团和多尼卡三方共同组建，专注于航空互联网运营与航空产业数字化，致力于打造数字客舱精品服务，提升 3.8 亿航空高净值人群出行体验。飞享互联集合了海航集团的航空运输能力、基地优势以及多尼卡在综合硬件解决方案方面的经验，底层支撑则来自于讯数字航空系列解决方案。飞享互联以封闭、独占的客舱场景互联网接入为入口，向飞行前、中、后全场景服务延伸，通过数字化建设为乘客提供信息流、音视频、游戏、广告、电商等多元娱乐出行服务；通过精准的航空客运流量，为驻地/目的地城市政府、景区、商家提供触达用户的数字化工具，实现航空+文旅数字化提升。目前飞享互联已取得国内 650+架飞机客舱互联网运营权，覆盖海航全机队，11 家航空公司。航线辐射一、二线重点城市消费群体，并向三、四线“支线”城市渗透，以迎合分级客群的多维需求。客舱局域网内，飞享互联为乘客提供了热门影视剧、头部短视频 IP、新闻资讯、音乐、游戏、漫画等丰富的文化和娱乐内容生态资源，打造机上 WiFi 内容全新体验。随着航空互联网产业快速发展，互联网、云计算、大数据和航空产业将会加速融合。整装待发的飞享互联公司将携手合作伙伴，为乘客提供品质服务、为航空产业创造价值。"
+        },
+        {
+            title: "主营业务",
+            words: "飞享互联公司打造的数字客舱精品服务，集合了海航集团的航空运输能力、基地优势以及多尼卡在综合硬件解决方案方面的经验，底层支撑则来自于腾讯数字客舱解决方案。在数字客舱产品中，腾讯融入了大数据和云计算技术优势、丰富的文化和娱乐内容生态资源。对乘客而言，客舱局域网能够满足观看影视剧、看新闻听歌打游戏等个性化娱乐需求；对航司而言，数字化意味着服务品质和效率的提升；对于运营商、软硬件厂商等行业合作伙伴而言，数字客舱是未来不容忽视的重要场景。  此外，腾讯还将建设开放平台，支持多渠道版权合作；并通过腾讯云CDN相关技术产品，如“筋斗云”，来加速机载数据更新，实现简化内容更新流程、节省内容更新人工成本，提高内容更新效率的目的。 飞享互联目前已取得国内650架飞机运营权。"
+        }
+    ],
+    companyAddress: [
+        {
+            company: "海南总公司",
+            address: "海南省海口市保税区海口综合保税区联检大楼四楼A1"
+        },
+        {
+            company: "上海分公司",
+            address: "上海市徐汇区虹梅路1535号2号楼701室"
+        },
+        {
+            company: "深圳分公司",
+            address: "广东省深圳市南山区南头街道安乐社区关口二路15号智恒产业园15栋1层"
+        }
+    ],
+    topNavItems: [
+        {
+            name: "1",
+            label: "首页",
+            scroll: "home-header"
+        },
+        {
+            name: "2",
+            label: "公司业务",
+            scroll: "business"
+        },
+        {
+            name: "3",
+            label: "产品介绍",
+            scroll: "product"
+        },
+        {
+            name: "4",
+            label: "新闻中心",
+            scroll: "news-center"
+        },
+        {
+            name: "5",
+            label: "合作伙伴",
+            scroll: "partners"
+        },
+        {
+            name: "6",
+            label: "关于我们",
+            scroll: "about-us"
+        },
+        {
+            name: "7",
+            label: "联系我们",
+            scroll: "contact-us"
+        }
+    ],
+    otherLinkItems: [
+        {
+            id: 1,
+            to: "/declaration",
+            label: "服务协议"
+        },
+        {
+            id: 2,
+            to: "/declaration",
+            label: "个人信息保护政策"
+        },
+        {
+            id: 3,
+            to: "/declaration",
+            label: "个人信息授权与保护声明"
+        },
+        {
+            id: 4,
+            to: "/declaration",
+            label: "儿童个人信息保护规则及监护人须知"
+        },
+        {
+            id: 5,
+            to: "/declaration",
+            label: "共享信息清单"
+        }
+    ]
 }
